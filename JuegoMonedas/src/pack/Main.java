ccd19e35d44bba678a96473812b075d9e2f6ff17
JoLuMo <jo_lumo@outlook.com>
2024-03-07 11:20:21 +0100

    main

---
+++ JuegoMonedas/src/pack/Main.java
@@ -0,0 +1,21 @@
+package pack;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//creamos un arraylist de monedas para ir guardando las monedas generadas
+		ArrayList<Moneda> secuencia = new ArrayList<>();
+		
+		
+		String[] valores = {"1 centimo", "2 centimos", "5 centimos", "10 centimos", "25 centimos", "50 centimos", "1 euro", "2 euros"};
+        String[] posiciones = {"cara", "cruz"};
+		
+        
+        
+	}
+
+}
---
+++ JuegoMonedas/src/pack/Moneda.java
@@ -0,0 +1,42 @@
+package pack;
+
+public class Moneda {
+
+	public String valor;
+	public String posicion;
+	
+	Moneda(String valor, String posicion){
+		
+		this.valor=valor;
+		this.posicion=posicion;
+		
+	}
+
+	
+	
+	
+	public String getValor() {
+		return valor;
+	}
+
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+
+	@Override
+	public String toString() {
+		return "Moneda [valor=" + valor + ", posicion=" + posicion + "]";
+	}
+	
+	
+	
+	
+}
